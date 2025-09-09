6aad5e7a66b256daef3ea226c87eac6e82bc41d3
KirneshWalunj <137167991+KirneshWalunj@users.noreply.github.com>
2025-09-09 11:14:37 +0530

    Small changes in index

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -96,12 +96,12 @@ const Home = () => {
           </div>
         </div>
 
-        {/* Scroll indicator */}
+        {/* Scroll indicator 
         <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
           <div className="w-6 h-10 border-2 border-white/40 rounded-full flex justify-center backdrop-blur-sm">
             <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse"></div>
           </div>
-        </div>
+        </div>*/}
       </section>
 
       {/* Featured Work Preview */}
